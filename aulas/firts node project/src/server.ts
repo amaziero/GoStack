76108bc5a841b751664533cea28f823ecb4ce4ae
amaziero <alison_lens@hotmail.com>
2020-10-28 19:29:59 -0300

    cofigured the eslint and editoconfig files

--- aulas/firts node project/src/server.ts
+++ aulas/firts node project/src/server.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello word" });
-});
+app.use(express.json());
+
+app.get('/', (request, response) => response.json({ message: 'Hello word' }));
 
 app.listen(3333, () => {
-  console.log("🚀 Server started");
+  console.log('🚀 Server started');
 });
