265861ee39b516f55dd208c4b5655f1c5f52d979
amaziero <alison_lens@hotmail.com>
2020-10-29 21:22:29 -0300

    creating the models and repositoties files

--- aulas/firts node project/src/routes/appointments.routes.ts
+++ aulas/firts node project/src/routes/appointments.routes.ts
@@ -1,29 +1,30 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-
-interface Appoitments {
-  id: string;
-  provider: string;
-  date: string;
-}
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appoitmentsRouter = Router();
-const appointments: Appoitments[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appoitmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
 
-  appointments.push(appointment);
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointInSameDate = appointmentsRepository.findByDate(parsedDate);
+
+  if (findAppointInSameDate) {
+    return response.status(400).json({
+      message: 'This hours is booked, please choose another hour',
+    });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
 
 appoitmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.findAll();
+
   return response.status(200).json(appointments);
 });
 
