f17f0265d65dde1341a6b12cca58e3a2d30246e6
amaziero <alison_lens@hotmail.com>
2020-11-10 19:47:29 -0300

    styles of the dashboard of the application

--- aulas/05-first-react-project/src/pages/Dashboard/index.tsx
+++ aulas/05-first-react-project/src/pages/Dashboard/index.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+import { Title, Form, Respositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositorios no github</Title>;
+  return (
+    <>
+      <img src={logo} alt="logo from github" />
+      <Title>Explore Repositorios no github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Respositories>
+        <a href="/">
+          <img
+            src="https://avatars1.githubusercontent.com/u/56273452?s=460&u=3fca3aa0712fefb70c9b87fa871797fd8341ee6f&v=4"
+            alt="foto"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Ease peasy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/">
+          <img
+            src="https://avatars1.githubusercontent.com/u/56273452?s=460&u=3fca3aa0712fefb70c9b87fa871797fd8341ee6f&v=4"
+            alt="foto"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Ease peasy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/">
+          <img
+            src="https://avatars1.githubusercontent.com/u/56273452?s=460&u=3fca3aa0712fefb70c9b87fa871797fd8341ee6f&v=4"
+            alt="foto"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Ease peasy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Respositories>
+    </>
+  );
 };
 
 export default Dashboard;
