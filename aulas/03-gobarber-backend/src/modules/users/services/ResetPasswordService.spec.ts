5c2b52f06fd495f943a5a7a8aa6004b1c4fde4a8
amaziero <alison_lens@hotmail.com>
2021-02-05 11:39:46 -0300

    test: added create hash functionality test

--- aulas/03-gobarber-backend/src/modules/users/services/ResetPasswordService.spec.ts
+++ aulas/03-gobarber-backend/src/modules/users/services/ResetPasswordService.spec.ts
@@ -1,20 +1,24 @@
 import FakeUsersRepositories from '../repositories/fakes/FakeUsersRepositories'
 import FakeUsersTokenRepositories from '../repositories/fakes/FakeUsersTokenRepositories'
 import ResetPassowrdService from './ResetPasswordService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 let fakeUsersRepository: FakeUsersRepositories;
 let fakeUserTokenRespository: FakeUsersTokenRepositories;
+let fakeHashProvider: FakeHashProvider;
 let resetPassowrd: ResetPassowrdService;
 
 describe('ResetPassowrd', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepositories();
     fakeUserTokenRespository = new FakeUsersTokenRepositories();
+    fakeHashProvider = new FakeHashProvider();
 
 
     resetPassowrd = new ResetPassowrdService(
       fakeUsersRepository,
-      fakeUserTokenRespository
+      fakeUserTokenRespository,
+      fakeHashProvider
     );
   })
 
@@ -25,15 +29,18 @@ describe('ResetPassowrd', () => {
       password: '123456',
     });
 
-    const userToken = await fakeUserTokenRespository.generate(user.id);
+    const { token } = await fakeUserTokenRespository.generate(user.id);
+
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash')
 
     await resetPassowrd.execute({
       password: '123123',
-      token: userToken.token,
+      token,
     })
 
     const updatedUser = await fakeUsersRepository.findById(user.id);
 
+    expect(generateHash).toHaveBeenCalledWith('123123')
     expect(updatedUser?.password).toBe('123123');
   });
 });
