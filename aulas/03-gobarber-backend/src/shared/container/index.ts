b5738e9397fb3c8c11b2d1f526baa7018c822de2
amaziero <alison_lens@hotmail.com>
2021-02-05 17:23:58 -0300

    refactor: ajusted names at the depency ingections file

--- aulas/03-gobarber-backend/src/shared/container/index.ts
+++ aulas/03-gobarber-backend/src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { container } from 'tsyringe';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepositories';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
 import IUsersRepository from '@modules/users/repositories/IUsersRepositories';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepositories';
+
 import IUsersTokenRepository from '@modules/users/repositories/IUserTokenRepositories';
 import UsersTokenRepository from '@modules/users/infra/typeorm/repositories/UsersTokensRepository';
 
