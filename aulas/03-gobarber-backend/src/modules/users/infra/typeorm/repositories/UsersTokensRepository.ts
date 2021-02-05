a1deab0387f1772bb7131203972a327046d7fc8d
amaziero <alison_lens@hotmail.com>
2021-02-05 16:54:08 -0300

    feat: CreateUserTokens migration

---
+++ aulas/03-gobarber-backend/src/modules/users/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,31 @@
+import UserToken from '../entities/UserToken';
+import { getRepository, Repository } from 'typeorm';
+import IUserTokenRepositories from '@modules/users/repositories/IUserTokenRepositories';
+
+class UsersTokenRepositories implements IUserTokenRepositories {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.ormRepository.findOne({
+      where: { token }
+    });
+
+    return userToken;
+  };
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.ormRepository.create({
+      user_id
+    })
+
+    await this.ormRepository.save(userToken)
+
+    return userToken
+  }
+}
+
+export default UsersTokenRepositories;
