3e04cbcd66267b6b48a94f0f92f50db5d00c7897
amaziero <alison_lens@hotmail.com>
2021-02-13 14:10:53 -0300

    refactor: fixed bug while creating dependency invertion

--- aulas/03-gobarber-backend/src/modules/users/providers/index.ts
+++ aulas/03-gobarber-backend/src/modules/users/providers/index.ts
@@ -2,4 +2,4 @@ import { container } from 'tsyringe';
 import IHasProvider from './HashProvider/models/IHashProvider';
 import BCryptHashProvider from './HashProvider/implementations/BCryptHashProvider'
 
-container.registerSingleton<IHasProvider>('HashProvider', BCryptHashProvider);
+container.registerInstance<IHasProvider>('HashProvider', container.resolve(BCryptHashProvider));
