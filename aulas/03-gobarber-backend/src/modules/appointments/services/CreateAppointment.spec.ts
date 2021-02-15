408313e0d7cb00df76a9a877760fafe900b3e449
amaziero <alison_lens@hotmail.com>
2021-02-15 10:52:59 -0300

    test: block the user to create and appointment after the passed hour

--- aulas/03-gobarber-backend/src/modules/appointments/services/CreateAppointment.spec.ts
+++ aulas/03-gobarber-backend/src/modules/appointments/services/CreateAppointment.spec.ts
@@ -11,10 +11,14 @@ describe('CreateAppointment', () => {
     createAppointment = new CreateAppoimentServices(fakeRepository);
   })
 
-  it('Should be able to create a new appimtment', async () => {
+  it('Should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime()
+    })
+
     const appointment = await createAppointment.execute({
       user_id: '6546549879871314679',
-      date: new Date(),
+      date: new Date(2020, 4, 10, 13),
       provider_id: '6546549879871314679'
     })
 
@@ -22,8 +26,12 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('6546549879871314679');
   });
 
-  it('Should not able to create tow appimtments at the same time', async () => {
-    const appointmentDate = new Date(2020, 10, 10, 11)
+  it('Should not able to create two appimtments at the same time', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime()
+    })
+
+    const appointmentDate = new Date(2020, 4, 10, 12)
 
     await createAppointment.execute({
       user_id: '6546549879871314679',
@@ -39,4 +47,16 @@ describe('CreateAppointment', () => {
       })
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('Should not be able to create an appointment in a passed time', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime()
+    })
+
+    expect(createAppointment.execute({
+      date: new Date(2020, 4, 10, 10),
+      provider_id: 'user',
+      user_id: 'provider_id'
+    })).rejects.toBeInstanceOf(AppError)
+  })
 });
