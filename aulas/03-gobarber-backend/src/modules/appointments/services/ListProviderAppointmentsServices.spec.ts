43501f1bdac4adca715b72c7de92edcbf2ee294d
amaziero <alison_lens@hotmail.com>
2021-02-15 17:13:55 -0300

    test: list appoiments in a day for an provider

---
+++ aulas/03-gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsServices.spec.ts
@@ -0,0 +1,36 @@
+import ListProviderAppointmentsServices from './ListProviderAppointmentsServices';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRespository'
+
+let listProviderAppointmentsServices: ListProviderAppointmentsServices;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository()
+    listProviderAppointmentsServices = new ListProviderAppointmentsServices(fakeAppointmentsRepository)
+  })
+
+  it(`Should be able to list the appointments in a day for a provider`, async () => {
+    const appointmentOne = await fakeAppointmentsRepository.create({
+      user_id: 'user_id',
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 12, 0, 0)
+    })
+
+    const appointmentTwo = await fakeAppointmentsRepository.create({
+      user_id: 'user_id',
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 13, 0, 0)
+    })
+
+    const appointments = await listProviderAppointmentsServices.execute({
+      provider_id: 'provider',
+      day: 20,
+      month: 5,
+      year: 2020
+    })
+
+    expect(appointments).toEqual([appointmentOne, appointmentTwo])
+  })
+})
